3bdc0b3afb902f94827f9767e4e0a792c014fb78
JackLovesData <83872339+JackLovesData@users.noreply.github.com>
2024-08-18 16:12:51 -0400

    install packages

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # install local package in my virtual env
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Jack Chen',
+    author_email='chenhongzhi90@gmail.com',
+    install_requires=["openai", "langchain",
+                      "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
